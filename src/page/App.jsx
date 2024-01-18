97b96d91f34eb4e43fb1ae33bd105f9e8a437ff1
Jason Selau Kumar <148664294+Mecha-Coder@users.noreply.github.com>
2024-01-18 17:56:00 +0800

    submit

---
+++ src/page/App.jsx
@@ -0,0 +1,14 @@
+import "../style/Main.css"
+import List from "../components/List"
+import Form from "../components/Form"
+
+function App() {
+  return (
+    <main>
+      <Form />
+      <List />
+    </main>
+  );
+}
+
+export default App;
